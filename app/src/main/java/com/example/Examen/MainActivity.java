be02d748a14771164535253aa941373d99f45854
tavo <gustavolvilorio04@hotmail.com>
2021-10-25 00:41:46 -0600

    primer comentario

---
+++ app/src/main/java/com/example/Examen/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.Examen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void clickNew(View view)
+    {
+        Intent intent = new Intent(this,ActivityContacto.class);
+        startActivity(intent);
+
+    }
+}
